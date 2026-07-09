55c76486f08de2f663484bf4bc55c056d47b3260
Siddharth <siddharth1212a@gmail.com>
2026-07-09 22:34:16 +0530

    Improved server

--- server/server.py
+++ server/server.py
@@ -1,10 +1,25 @@
 from server.socket_handler import SocketServer
 
+
 def main():
+
+    print("=" * 55)
+    print("        🚀 Welcome To PyConnect Server")
+    print("=" * 55)
 
     server = SocketServer()
 
-    server.start()
+    try:
+
+        server.start()
+
+    except KeyboardInterrupt:
+
+        print("\n🛑 Server Stopped Successfully.")
+
+    except Exception as e:
+
+        print(f"\n❌ Server Error : {e}")
 
 
 if __name__ == "__main__":
